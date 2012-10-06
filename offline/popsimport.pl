3746435da58d5ea09dc3a5836e6cca4c6cd385bb
Josh Gitlin <joshua.gitlin@razorfish.com>
2012-10-06 08:58:26 -0400

    migrate from hg/bitbucket
    I don't want to, but build.phonegap.com only suports github. I don't
    like git. The fast-export tool failed miserably, so I'm just abandoning
    my hg commit history. C'est la vie.

---
+++ offline/popsimport.pl
@@ -0,0 +1,72 @@
+use strict;
+use warnings;
+use feature ':5.16';
+
+use Archive::Extract;
+use Log::Message::Simple qw[msg error debug croak carp cluck confess];
+use LWP::Simple;
+
+use constant VERBOSE => 1;
+use constant POPS_URL => "https://nycopendata.socrata.com/download/fum3-ejky/application/zip";
+use DBI;
+use DBD::ODBC;
+
+use Data::Dumper;
+use Text::xSV;
+use JSON;
+use LWP::UserAgent;
+
+open my $file, ">data.json";
+my $csv = Text::xSV->new();
+$csv->open_file("tbl_POPS.txt");
+$csv->read_header();
+while ($csv->get_row()) {
+	my ($address, $name, $location, @ps) = $csv->extract(
+		"Building Address","Building Name","Building Location","Public Space 1","PS 1 Size","Public Space 2","PS 2 Size","Public Space 3","PS 3 Size","Public Space 4","PS 4 Size","Public Space 5","PS 5 Size");
+	$name ||= "";
+	my %spaces = map {$_ ? $_ : ""} @ps;
+	
+	my $ua = LWP::UserAgent->new();
+	msg("Fetching " . $address, VERBOSE);
+	my $address_param = $address . " New York, NY" =~ s/ /+/gr;
+	my $success = 0;
+	my $tries = 0;
+	while (!$success) {
+		my $response = $ua->get("http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=" . $address_param);
+		if ($response->is_success) {
+			my $geodata = from_json $response->decoded_content;
+			if ($geodata->{'status'} eq 'OVER_QUERY_LIMIT') {
+				$tries++;
+				croak("GMap daily query limit exceeded") if $tries == 3;
+				carp("Querying too fast, try again in 2s", VERBOSE);
+				sleep 2;
+				next;
+			} elsif ($geodata->{'status'} eq 'OK') {
+				msg("Query returned OK", VERBOSE);
+				$success = 1;
+				say $file to_json({'name'=>$name,'address'=>$address,'location'=>$location,"spaces"=>\%spaces, "geodata" => $geodata}, {pretty=>1});
+			} else {
+				croak("GMaps returned bad status" . $geodata->{'status'});
+			}
+		}
+		else {
+			croak("GMap returned bad HTTP response: " . $response->status_line);
+		}
+	}
+}
+
+
+sub fetch_data {
+	my $zip_stash = "pops.zip";
+	my $mdb_stash = "popsdb";
+	msg("Fetching " . POPS_URL, VERBOSE);
+	my $data = getstore(POPS_URL, $zip_stash);
+	msg("Inflating zip", VERBOSE);
+	my $mdbzip = Archive::Extract->new(archive => $zip_stash);
+	unlink $mdb_stash;
+	my $ok = $mdbzip->extract(to => $mdb_stash);
+	unlink $zip_stash;
+	die unless $ok;
+	msg("MDB created at " . $mdb_stash, VERBOSE);
+	return glob("$mdb_stash/*.mdb");
+}
---
+++ src/js/sitonit.js
@@ -0,0 +1,120 @@
+var sitonit = {}; // namespace
+sitonit.map = function() { // main class for the map of POPs
+	var map; // reference to google.maps.Map object
+	var geocoder = new google.maps.Geocoder();
+	var markers = []; // store list of placed markers because Google API doesn't do it for you
+	
+	// thank you Pythagoras
+	function computeDistance(pointA, pointB) {
+		return Math.pow(pointB.lat()-pointA.lat(), 2) + Math.pow(pointB.lng()-pointA.lng(), 2);
+	}
+	
+	// maintain list of POPs in sorted order with a fixed limit
+	function insertSorted(list, item, limit) {
+		var i = 0;
+		while (i < list.length) {
+			if (item.distance < list[i].distance) {
+				// item is smaller than an item in the list, so insert it
+				list.splice(i, 0, item);
+				break;
+			}
+			i++;
+		}
+		if (list.length < limit) {
+			// not yet at limit, so put it on the end
+			list.push(item);
+			return true;
+		} else if (list.length > limit) {
+			// inserted an item that expanded the list over the limit, so drop the highest value
+			list.pop();
+			return true;
+		}
+		return false;
+	}
+	// Search the DB for nearest POPs from center point
+	function findClosest(center/* LatLng*/, callback) {
+		console.log(center);
+		var limit = 10;
+		var closest = [];
+		$(pops).each(function(i, pop) {
+			var popCoords = pop.geodata.results[0].geometry.location; // {lat:,lng:}
+			console.log("Computing distance to " + pop.address + "[" + popCoords.lat + "," + popCoords.lng + "]");
+			var dist = computeDistance(center, new google.maps.LatLng(popCoords.lat, popCoords.lng));
+			console.log("Distance is " + dist);
+			pop.distance = dist;
+			insertSorted(closest, pop, limit);
+		});
+		console.log(closest);
+		callback(closest);
+
+	}
+	var openWindows = [];
+	function openWindow(marker) {
+		closeWindows();
+		var infoWindow = marker.info;
+		infoWindow.open(marker.map, marker);
+		openWindows.push(infoWindow);
+	}
+	function closeWindows() {
+		$(openWindows).each(function(i, win) {
+			win.close();
+		});
+	}
+	return {
+		initMap: function(div) {
+			console.log("Adding map to " + div);
+		    var mapOptions = {
+		      center: new google.maps.LatLng(40.730218,-73.973351),
+		      zoom: 13,
+		      mapTypeId: google.maps.MapTypeId.ROADMAP
+		    };
+		    map = new google.maps.Map(div, mapOptions);
+		    console.log(map);
+		    return this;
+		},
+		addPop: function(pop) {
+			if (!map) return;
+			var coords = pop.geodata.results[0].geometry.location;
+			var marker = new google.maps.Marker({
+				map: map,
+				position: new google.maps.LatLng(coords.lat, coords.lng),
+				title:(pop.name ? pop.name + " " : "") + pop.address
+			});
+			marker.info = new google.maps.InfoWindow({
+				  content: (pop.name ? "<h4>" + pop.name + "</h4>" : "") +
+				  	pop.address + "<br/>" + 
+				  	(pop.location ? pop.location : "")
+				});
+			google.maps.event.addListener(marker, 'click', function() {
+					openWindow(marker);
+				});
+			markers.push(marker);
+		},
+		findByAddress: function(address, callback) {
+			if (address) {
+				geocoder.geocode( { 'address': address}, function(results, status) {
+					console.log("Status: " + status);
+					var coords = results[0].geometry.location; // LatLng object
+					findClosest(coords, callback);
+				});
+			} else {
+				navigator.geolocation.getCurrentPosition(function(position) {
+					findClosest(new google.maps.LatLng(position.coords.latitude, position.coords.longitude), callback);
+				});
+			}
+		},
+		setBoundsByMarkers: function() {
+			var bounds = new google.maps.LatLngBounds();
+			$(markers).each(function(i, marker) {
+				bounds.extend(marker.position);
+			});	
+			map.fitBounds(bounds);
+		},
+		clearMarkers: function() {
+			for(var i = 0; i < markers.length; i++) {
+				markers[i].setMap(null);
+			}
+			markers = [];
+		}
+	};
+};
