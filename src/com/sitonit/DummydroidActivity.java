48b7d0cc52721f2d2b4ab76e82840fcbbfdf68d3
Josh Gitlin <joshua.gitlin@razorfish.com>
2012-10-07 23:18:12 -0400

    Setup as runnabe Android app

---
+++ src/com/sitonit/DummydroidActivity.java
@@ -0,0 +1,13 @@
+package com.sitonit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DummydroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
